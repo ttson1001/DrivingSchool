805f0fda38ffc3aac7b9428151765f2084d0398c
ttson1001 <74534768+ttson1001@users.noreply.github.com>
2025-12-08 22:17:57 +0700

    okay

--- TutorDrive/Repositories/Repository.cs
+++ TutorDrive/Repositories/Repository.cs
@@ -19,7 +19,7 @@ namespace TutorDrive.Repositories
 
         public IQueryable<T> Get()
         {
-            return _set.Where(x => true).AsNoTracking();
+            return _set.Where(x => true);
         }
 
         public async Task<List<T>?> GetValuesAsync(CancellationToken cancellationToken = default)
